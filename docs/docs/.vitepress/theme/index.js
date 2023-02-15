0c3aa3dfd20386d07c08489ae957efaf574e6573
etheral12138 <1694815257@qq.com>
2023-02-16 02:45:43 +0800

    初始化

--- docs/docs/.vitepress/theme/index.js
+++ docs/docs/.vitepress/theme/index.js
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 // import EtDesign from 'et-design'
+
 // 插件的组件，主要是demo组件
 import './styles/index.css'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
