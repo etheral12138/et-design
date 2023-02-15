70798a4a6234c190eac26940cf3954871a14f2de
etheral12138 <1694815257@qq.com>
2023-02-16 02:28:48 +0800

    初始化

--- docs/docs/.vitepress/theme/index.js
+++ docs/docs/.vitepress/theme/index.js
@@ -1,6 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
-import EtDesign from 'et-design'
-import 'et-design/dist/index.css'
+// import EtDesign from 'et-design'
+// import 'et-design/dist/index.css'
 // 插件的组件，主要是demo组件
 import './styles/index.css'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
@@ -8,10 +8,16 @@ import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue'
 import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue'
 
 export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.use(EtDesign)
-    app.component('Demo', Demo)
-    app.component('DemoBlock', DemoBlock)
-  }
+    ...DefaultTheme,
+    enhanceApp({app}) {
+        app.mixin({
+            mounted() {
+                import('et-design').then(function (m) {
+                    app.use(m.default)
+                })
+            },
+        }),
+            app.component('Demo', Demo)
+        app.component('DemoBlock', DemoBlock)
+    }
 }
